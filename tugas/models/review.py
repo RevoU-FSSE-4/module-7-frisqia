9359d713f65a10ab175812e1f2896022c24c8f25
frisqia fatmalaningsih <frifani123@gmail.com>
2024-06-29 00:33:58 +0700

    test

---
+++ tugas/models/review.py
@@ -0,0 +1,16 @@
+from models.base import Base
+from sqlalchemy import Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy.sql import func
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = mapped_column(Integer, primary_key=True)
+    username = mapped_column(Integer, ForeignKey("product.id", ondelete="CASCADE"))
+    # product_id = mapped_column(Integer)
+    email = mapped_column(String(30), nullable= False)
+    rating = mapped_column(Integer)
+    review_content = mapped_column(Text)
+    created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
+    
